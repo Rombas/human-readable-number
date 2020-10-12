99891fe793b6b7735e0c68d6f2244835f78b8e79
RomanBaraban <Romanbash@gmail.com>
2020-10-12 12:16:34 +0300

    feat: realize Human Readable Number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
+    if (number === 0) return "zero";
+
+    var a = ['','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety']; 
+    let res = number.toString();
+    let answer = [];
+    
+    while (res.length < 3) {    
+        res = "0" + res;
+    }
+
+    if (res[0] !== "0") {
+        answer.push(a[Number.parseInt(res[0])]);
+        answer.push("hundred");
+    }
+    
+    if (res[1] > 1) { 
+        answer.push(b[Number.parseInt(res[1])]);
+        
+        if (res[2] !== "0"){
+        answer.push(a[Number.parseInt(res[2])]);
+        }
+    }
+    else if ((res[1] + res[2]) !== "00") {
+        answer.push(a[Number.parseInt(res[1]+res[2])]);
+    }
+    
+    return answer.join(" ");
 }
+
